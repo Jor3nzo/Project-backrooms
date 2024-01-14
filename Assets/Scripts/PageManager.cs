b5c1162a82783d69b689166645ac538058181c93
k <135144670+Jor3nzo@users.noreply.github.com>
2024-01-14 13:08:43 +0200

    Clipboards model

---
+++ Assets/Scripts/PageManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PageManager : MonoBehaviour
+{
+	public Enemy enemy;
+	public int pagesCollected = 0;
+
+	void OnTriggerEnter(Collider other)
+	{
+		print("page collected");
+		Destroy(other.gameObject);
+
+		pagesCollected++;
+
+		if (pagesCollected == 1)
+		{
+			enemy.target = transform;
+		}
+
+		if (pagesCollected == 2)
+		{
+			enemy.speed *= 2;
+		}
+
+		if (pagesCollected == 3)
+		{
+			enemy.viewRange = 15;
+		}	
+	}
+}
